fb10bd54d217b79b843f162aaaa61442c536e171
dmkirshon <kirshon.david@gmail.com>
2022-01-28 18:39:33 +0200

    feat: Add ability to enter player names

--- js/script.js
+++ js/script.js
@@ -43,7 +43,19 @@ const player = (name, symbol) => {
 // display object
 
 const displayController = (() => {
-    
+    let inputPlayerOne;
+    let inputPlayerTwo;
+
+    const startGameForm = document.querySelector('.form-start-game');
+    startGameForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        inputPlayerOne = document.getElementById('playerOne').value;
+        inputPlayerTwo = document.getElementById('playerTwo').value;
+        startGameForm.reset();
+        startGameForm.hidden = true;
+        playGame.init();
+    });
+
     const drawBoard = () => {
         gameBoard.getBoard().forEach( (spot, i) => {
             if(spot) {
@@ -57,107 +69,111 @@ const displayController = (() => {
         const messageArea = document.querySelector('.game-messages');
         messageArea.textContent = message;
     };
-    return {drawBoard, displayMessage};
+
+    const getInputtedPlayerOne = () => inputPlayerOne;
+    const getInputtedPlayerTwo = () => inputPlayerTwo;
+
+    return {drawBoard, displayMessage, getInputtedPlayerOne, getInputtedPlayerTwo};
 })();
 
 // game object
     // game logic, win logic, game message 
-    const playGame = (() => {
+const playGame = (() => {
+    let currentPlayer;
+    let otherPlayer;
+    let gameOver = false;
+    let gameOutcomeIsWin = false;
 
-        displayController.drawBoard();
-        const playerOne = player('Player 1', 'x');
-        const playerTwo = player('Player 2', 'o');
-        let currentPlayer = playerOne;
-        let otherPlayer = playerTwo;
-        let gameOver = false;
-        let gameOutcomeIsWin = false;
-    
-        const init = () => {
-            pickSpot();
-        };
+    const init = () => {
+        const playerOne = player(displayController.getInputtedPlayerOne(), 'x');
+        const playerTwo = player(displayController.getInputtedPlayerTwo(), 'o');
+        currentPlayer = playerOne;
+        otherPlayer = playerTwo;
+        pickSpot();
+    };
 
-        const pickSpot = () => {
-            const currentSymbol = currentPlayer.getSymbol();
-    
+    const pickSpot = () => {
+        const currentSymbol = currentPlayer.getSymbol();
+
+        Array.from(gameBoardGrid.children).forEach(spot => {
+            if(!(spot.textContent)) {
+            spot.addEventListener('click', selectSpot);
+            spot.classList.add('hoverOnSpot');
+            }
+        });
+
+        function selectSpot() {
+            gameBoard.setSpot(currentSymbol, this.getAttribute('data-spot-number'));
             Array.from(gameBoardGrid.children).forEach(spot => {
-                if(!(spot.textContent)) {
-                spot.addEventListener('click', selectSpot);
-                spot.classList.add('hoverOnSpot');
-                }
+                spot.removeEventListener('click', selectSpot);
+                spot.classList.remove('hoverOnSpot');
             });
-    
-            function selectSpot() {
-                gameBoard.setSpot(currentSymbol, this.getAttribute('data-spot-number'));
-                Array.from(gameBoardGrid.children).forEach(spot => {
-                    spot.removeEventListener('click', selectSpot);
-                    spot.classList.remove('hoverOnSpot');
-                });
-                displayController.drawBoard();
+            displayController.drawBoard();
 
-                if(checkWin()){
-                    gameOutcomeIsWin = true;
-                    endGame();
-                } else if (checkTie()) {
-                    endGame();
-                } else{
-                    const swapPlayer = currentPlayer;
-                    currentPlayer = otherPlayer;
-                    otherPlayer = swapPlayer;
-                    pickSpot();
-                }
-            };
-        };
-
-        const checkWin = () => {
-            const currentBoard = gameBoard.getBoard();
-            if((currentBoard[0] && currentBoard[0] === currentBoard[1] && currentBoard[1] === currentBoard[2]) ||
-            (currentBoard[0] && currentBoard[0] === currentBoard[3] && currentBoard[3] === currentBoard[6]) ||
-            (currentBoard[0] && currentBoard[0] === currentBoard[4] && currentBoard[4] === currentBoard[8]) ||
-            (currentBoard[1] && currentBoard[1] === currentBoard[4] && currentBoard[4] === currentBoard[7]) ||
-            (currentBoard[2] && currentBoard[2] === currentBoard[4] && currentBoard[4] === currentBoard[6]) ||
-            (currentBoard[2] && currentBoard[2] === currentBoard[5] && currentBoard[5] === currentBoard[8]) ||
-            (currentBoard[3] && currentBoard[3] === currentBoard[4] && currentBoard[4] === currentBoard[5]) ||
-            (currentBoard[6] && currentBoard[6] === currentBoard[7] && currentBoard[7] === currentBoard[8])) {
-                return true;
-            } else {
-                return false;
+            if(checkWin()){
+                gameOutcomeIsWin = true;
+                endGame();
+            } else if (checkTie()) {
+                endGame();
+            } else{
+                const swapPlayer = currentPlayer;
+                currentPlayer = otherPlayer;
+                otherPlayer = swapPlayer;
+                pickSpot();
             }
         };
+    };
 
-        const checkTie = () => {
-            const currentBoard = gameBoard.getBoard();
-            if (currentBoard.includes(undefined)) {
-                return false;
-            } else {
-                return true;
-            }
+    const checkWin = () => {
+        const currentBoard = gameBoard.getBoard();
+        if((currentBoard[0] && currentBoard[0] === currentBoard[1] && currentBoard[1] === currentBoard[2]) ||
+        (currentBoard[0] && currentBoard[0] === currentBoard[3] && currentBoard[3] === currentBoard[6]) ||
+        (currentBoard[0] && currentBoard[0] === currentBoard[4] && currentBoard[4] === currentBoard[8]) ||
+        (currentBoard[1] && currentBoard[1] === currentBoard[4] && currentBoard[4] === currentBoard[7]) ||
+        (currentBoard[2] && currentBoard[2] === currentBoard[4] && currentBoard[4] === currentBoard[6]) ||
+        (currentBoard[2] && currentBoard[2] === currentBoard[5] && currentBoard[5] === currentBoard[8]) ||
+        (currentBoard[3] && currentBoard[3] === currentBoard[4] && currentBoard[4] === currentBoard[5]) ||
+        (currentBoard[6] && currentBoard[6] === currentBoard[7] && currentBoard[7] === currentBoard[8])) {
+            return true;
+        } else {
+            return false;
         }
-        
-        const endGame = () => {
-            gameOver = true;
-            if (gameOutcomeIsWin) {
-                currentPlayer.giveWinPoint();
-                gameMessage();
+    };
+
+    const checkTie = () => {
+        const currentBoard = gameBoard.getBoard();
+        if (currentBoard.includes(undefined)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+    
+    const endGame = () => {
+        gameOver = true;
+        if (gameOutcomeIsWin) {
+            currentPlayer.giveWinPoint();
+            gameMessage();
+        } else {
+            gameMessage();
+        }
+    };
+
+    const gameMessage = () => {
+            let messageString = '';
+            if(gameOver && gameOutcomeIsWin) {
+                messageString = `You win ${currentPlayer.getName()}!
+                The current score is
+                ${currentPlayer.getName()}: ${currentPlayer.getScore()}
+                ${otherPlayer.getName()}: ${otherPlayer.getScore()}`;
             } else {
-                gameMessage();
+                messageString = `The game is a tie!
+                The current score is
+                \n${currentPlayer.getName()}: ${currentPlayer.getScore()}
+                ${otherPlayer.getName()}: ${otherPlayer.getScore()}`;
             }
+            displayController.displayMessage(messageString);
         };
 
-        const gameMessage = () => {
-                let messageString = '';
-                if(gameOver && gameOutcomeIsWin) {
-                    messageString = `You win ${currentPlayer.getName()}!
-                    The score is ${currentPlayer.getName()}: ${currentPlayer.getScore()}
-                    ${otherPlayer.getName()}: ${otherPlayer.getScore()}`;
-                } else {
-                    messageString = `The game is a tie!
-                    The score is ${currentPlayer.getName()}: ${currentPlayer.getScore()}
-                    ${otherPlayer.getName()}: ${otherPlayer.getScore()}`;
-                }
-                displayController.displayMessage(messageString);
-            };
-
-        return {init};
-    })();
-
-playGame.init();
+    return {init};
+})();
